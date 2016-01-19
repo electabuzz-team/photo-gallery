73a4b589cb8250324fb0b7452584126d54fa46e7
Toan Vo <toan@tinypulse.com>
2016-01-19 20:36:24 +0700

    scaffold photo

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bcrypt', '~> 3.1.7'
+gem 'geokit-rails'
 
 group :development do
   gem 'web-console', '~> 2.0'
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160119133147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "photos", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.boolean  "published"
+    t.string   "storage_url"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
