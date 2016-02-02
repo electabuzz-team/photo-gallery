8a350552ccab6f84bcbd6e9791800970de71b149
Thanh Dong <thanh.dqblue@gmail.com>
2016-02-02 10:00:54 +0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
 	get 'search' => 'photos#search'
-	post 'search_result' => 'photos#search_photo'
+  post 'search_result' => 'photos#search_photo'
+  post 'search_result_json' => 'photos#search_photo_json'
 end
