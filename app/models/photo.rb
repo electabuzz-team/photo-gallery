21238fe936d895c9966bc9e3ba71efd3eaa9a236
Thanh Dong <thanh.dqblue@gmail.com>
2016-02-02 11:29:50 +0700

    update search map

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ActiveRecord::Base
   has_attached_file :photo, styles: { medium: "100x100>", thumb: "20x20>" },
-                    :storage => :dropbox,
-                    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+                    :storage => :dropbox
+                    # :dropbox_credentials => Rails.root.join("config/dropbox.yml")
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
   acts_as_mappable :lat_column_name => :latitude,
                    :lng_column_name => :longitude
